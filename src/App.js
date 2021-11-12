727a09fda77bd86513303e4111cad5deacec62c6
Corned <Corned@users.noreply.github.com>
2021-11-13 00:51:09 +0200

    done :D

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import { RiMeteorLine } from "react-icons/ri";
+import "./App.css"
 
-function App() {
+const Mine = ({ handleClick, isMine, mineSelected}) => {
+  const isFail = isMine || (mineSelected&&Math.random()<0.2)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`cell ${isFail && "fail"}`} onClick={handleClick}>
+      { isFail &&
+        <RiMeteorLine/>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const [ width, setWidth ] = useState(10)
+  const [ height, setHeight ] = useState(10)
+  const [ mine, setMine ] = useState(-1)
+
+  const handleClick = (location) => {
+    if (mine > 0) return
+
+    setMine(location)
+  }
+
+  const restart = () => {
+    setMine(-1)
+  }
+
+  return (
+    <div className="App">
+      
+      <h1>Minesweeber</h1>
+      <div className="grid" style={{ gridTemplateColumns: `repeat(${width}, 50px)`, gridAutoRows: 50 }}>
+        {
+          Array.from({ length: width * height }).map((_, i) => (
+            <Mine isMine={i === mine} mineSelected={mine > 0} handleClick={() => handleClick(i)}/>
+          ))
+        }
+      </div>
+
+      { mine > 0 &&
+        <button className="reset" onClick={restart}>reset</button>
+      }
+
+      <footer>
+        <a alt="lol" href="https://www.youtube.com/watch?v=M5V_IXMewl4">made with 💋 by Tempo</a>
+      </footer>
+
+    </div>
+  )
+}
+
+export default App
